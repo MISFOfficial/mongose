3fd18615040a4f2d7163fa343c8406d3dd3ecfe5
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-12-17 22:01:12 +0600

    create interface to defiend data type

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,47 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { Schema } from 'mongoose'
 
 const app: Application = express()
 
 app.use(cors())
 
+// perse data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Running')
+    // res.send('Server is Running')
+
+    // testing a sample data to for prectice
+    // step 1 : create a interface
+    // step 1 : Schema
+    // step 1 : Model
+    // step 1 : Database Query
+
+
+    // create a interface
+    interface IUser {
+        id: string;
+        role: 'admin';
+        password: string;
+        name: {
+            firstName: string;
+            middleName: string;
+            lastName: string;
+        };
+        gender: 'male' | 'female';
+        email?: string
+    }
+
+    // creating schema using interface
+    const userSchema = new Schema<IUser>({
+        id: { type: String, required: true, unique: true },
+        role: { type: String, required: true },
+        password: { type: String, required: true },
+        name: { type: String, required: true },
+
+    })
 })
 
 
