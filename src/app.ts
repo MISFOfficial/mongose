f88f49006659f9dd4b5954379bb80dec3c3a60ae
misfofficial <muksitul44@gmail.com>
2025-12-23 18:40:09 +0600

    dkfhaksd

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,11 @@
-import mongoose from "mongoose";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-const express = require("express");
-const app = express();
-const port = 5000;
+const app: Application = express();
+app.use(cors());
 
-// data base connections
-
-async function main() {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/statefast");
-    console.log("Database connection successfully");
-  } catch (err) {
-    console.log("Conection Faild!", err);
-  }
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port = 5000;
+
+// data base connections
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb://admin:secret123@localhost:27017/?authSource=admin"
+    );
+    console.log("Database connection successfully");
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log("Conection Faild!", err);
+  }
+}
+
+main();
